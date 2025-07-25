dfaeb2a0e207c6a39254fcf5e69713b94d9b2694
Antonio Navarro <dojeDulDev@Proton.me>
2025-07-25 09:26:54 +0200

    feat(expenses): validate required fields when creating a new expense and show error using React-Toastify

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
-# React + Vite
+![Interfaz de usuario](./img/DashBoard-[screen-shoot].jpg)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# Estados
 
-## Expanding the ESLint configuration
+| #  | Name              | Detalle                                                       |
+|----|-------------------|---------------------------------------------------------------|
+| 1  | createExpenseBtn  | Llama a: `createNewExpense`                                   |
+| 2  | showSettingsBtn   | Muestra y oculta la ventana `setIsOpenSettings(!isOpenSettings)` |
+| 3  | totalWeekExpense  |                                                               |
+| 4  | weeklyLimit       | Estado. Se obtiene en `getData`                               |
+| 5  | expenses          |                                                               |
+| 6  | pagination        |                                                               |
 
-If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+---
+
+## Botones de acción
+
+| Botón              | ID                  | Función                         |
+|--------------------|---------------------|----------------------------------|
+|                    | editExpenseButton    | `editExpense(expense._id)`       |
+|                    | deleteExpenseButton  | `deleteExpense(expense._id)`     |
+| saveExpense        |                     | `saveExpense(expense)`          |
+| cancelExpenseEdit  |                     | `cancelEditExp`                 |
--- src/pages/ExpenseTrackerPage.jsx
+++ src/pages/ExpenseTrackerPage.jsx
@@ -2,13 +2,18 @@ import { FiPlus, FiSettings, FiEdit2, FiTrash2, FiChevronLeft, FiChevronRight, F
 import { useState, useEffect } from "react";
 import SettingsSideBar from './SettingsSideBar'
 import {axiosInstance} from '../util/axios';
+import { ToastContainer, toast } from 'react-toastify';
+
 
 const ExpenseTrackerPage = () => {
 
+  // Muestra | Oculta la ventana de configuración
   const [isOpenSettings, setIsOpenSettings] = useState(false);
   
+  // Identifica el registro editardo por su id
   const [editExpenseId, setEditExpenseId] = useState(null);
 
+  // Muestra | Oculta una nueva fila al inicio de la tabla para insertar el nuevo gasto. 
   const [newExpenseRow, setnewExpenseRow] = useState(false)
   
   const [newExpesense, setNewExpense] = useState({
@@ -37,12 +42,6 @@ const ExpenseTrackerPage = () => {
         setExpenses(data.expenses)
         setCategories(data.categories)
         
-        // console.log ('weeklyLimit ', weeklyLimit)
-        // console.log ('categories', categories)
-        // console.log ('expenses ', expenses)
-
-        
-        
 
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -56,7 +55,7 @@ const ExpenseTrackerPage = () => {
   }, [])
   
   
-
+  const showError = (message) => toast.error(message);
 
   const editExpense = (id) => {    
     setEditExpenseId(id)
@@ -85,20 +84,75 @@ const ExpenseTrackerPage = () => {
 
   
 
-  const saveExpense = () => {    
-    setEditExpenseId(null)   
+  const saveExpense = (expense) => {    
+
+    
+    const validExpense = validateExpenseData(expense)
+
+    if (validExpense) {
+      // Alta en la base de datos
+      setEditExpenseId(null)
+    }  
+
+       
 
 
   }
 
+
+  const validateExpenseData = (expense) => {
+  
+  //   {
+  //   "_id": "6881f1a4630243bf6c64ac1c",
+  //   "date": "2025-07-25",
+  //   "concept": "Rotulador",
+  //   "category": "Compras",
+  //   "amount": 2 
+  // }
+
+  const {date, concept, category, amount } = expense
+
+  console.log('concept ', concept)
+  
+  if (!date || !date.trim()) {
+    console.log('La fecha es obligatoria');
+    showError("La fecha es obligatoria")
+    return false;
+  }
+  if (!concept || !concept.trim()) {
+    console.log('El concepto es obligatorio');
+    showError("El concepto es obligatorio")
+    return false;
+  }
+  if (!category || !category.trim()) {
+    console.log('La categoría es obligatoria');
+    showError("La categoría es obligatoria")
+    return false;
+  }
+
+  if (amount <= 0) {
+    console.log('La cantidad tiene que se mayor que cero');
+    showError("La cantidad tiene que se mayor que cero")
+    return false;
+  }  
+
+  return true
+}
+
+  // Si se cancela la edicion del registro se busca en el array original y sustituye por el valor que tenia: editedExpense
+
+  
+
   const cancelEditExp = () => {    
-
-
+      // 1) Se busca en el estado gastos (expenses) el registro por su id y si lo encuentra
+      //    se sustituye por el valor que tenia. En el resto de casos se deja el que estaba. 
       setExpenses(expenses.map(expense => 
       expense.id === editExpenseId 
       ? editedExpense
       : expense
     ))
+    // Se reinicia el id del gastos a editar. 
+    // Se deshabilita la edición para que la fila no sea editable. 
     setEditExpenseId(null)
     setEditedExpense(null)
 
@@ -129,10 +183,19 @@ const ExpenseTrackerPage = () => {
   }
   
   // ✔️  Button
-  const addExpense = () => {    
-    setExpenses(prevExpenses => [...prevExpenses, newExpesense]);
-    setNewExpense({...newExpenseRow, id: expenses.length + 1})
-    setnewExpenseRow(false)
+  const addExpense = async () => {
+    try {
+      console.log ('newExpesense ', newExpesense)
+      const {data: created} = await axiosInstance.post('/expenses/', newExpesense);    
+      setExpenses(prevExpenses => [...prevExpenses, created]);
+      setnewExpenseRow(false)
+    } catch (error) {
+      console.error("Error al crear gasto:", error);
+    }
+      
+    
+    // setNewExpense({...newExpenseRow, id: expenses.length + 1})
+    
   }
 
   const cancelAddNewExpense = () => {
@@ -158,19 +221,23 @@ const ExpenseTrackerPage = () => {
   
 
   return (
+    
     <div className="min-h-scren bg-gray-50 p-6">
         <div className='max-w-6xl mx-auto'>
+          <ToastContainer position="top-right" type="error" />
 {/* Header and buttons*/}
           <div className="flex justify-between items-center mb-8">
                 <h1 className='text-6xl'> Expense Tracker </h1>                
                 <div className="flex gap-2" id="buttons">
                     <button 
+                      id="createExpenseBtn"
                       className="bg-blue-500 rounded-full text-white cursor-pointer p-2"
                       onClick={ () => createNewExpense()  }
                       >
                       <FiPlus className="w-8 h-8 cursor-pointer"/>
                     </button>
                     <button 
+                      id="showSettingsBtn"
                       className="bg-gray-500 rounded-full text-white cursor-pointer p-2"
                       onClick ={() => setIsOpenSettings(!isOpenSettings) }>
                       <FiSettings className="w-8 h-8"/>
@@ -200,6 +267,8 @@ const ExpenseTrackerPage = () => {
               </div>            
           </div>
 {/* Expense Table */}
+          {expenses.length > 0 || newExpenseRow ?           
+          
           <table className="w-full bg-white rounded-lg shadow-md mt-8 text-left">
               <tbody className="bg-white divide-y divide-gray-200"> 
                 <th className="px-6 py-3 "> Fecha </th>
@@ -207,7 +276,7 @@ const ExpenseTrackerPage = () => {
                 <th> Categoria </th>
                 <th> Cantidad </th>
                 <th> Acciones </th>
-
+                  {/* Muestra la primera fila en modo edición para agregar el gasto. */}
                   {newExpenseRow &&
                       <> 
                         <tr> 
@@ -226,7 +295,7 @@ const ExpenseTrackerPage = () => {
                           <td > <input type="number" name="amount" className="border" onChange={editNewExpenseData} /> </td>  
                           <td className=""> 
                             <div className="flex space-x-2">
-                              <button onClick={() => addExpense()}>	
+                              <button id="confirmNewExpens" onClick={() => addExpense()}>	
                                 <FiCheck className="text-green-500 w-5 h-5"/> 
                                 </button>                          
                               <button onClick={() => cancelAddNewExpense()}>
@@ -277,7 +346,8 @@ const ExpenseTrackerPage = () => {
                         <div className="flex space-x-2">
                           {editExpenseId != expense._id ?
                           <> 
-                          <button
+                          <button 
+                            id="editExpenseButton"
                             onClick={() => editExpense(expense._id)}
                           >
                             <FiEdit2 
@@ -285,6 +355,7 @@ const ExpenseTrackerPage = () => {
                           </button>
                           
                           <button
+                            id="deleteExpenseButton"
                             onClick={() => deleteExpense(expense._id)}
                           > 
                             <FiTrash2 className="text-red-500 w-5 h-5"/>
@@ -295,13 +366,15 @@ const ExpenseTrackerPage = () => {
                         </>
                         :  
                         <> 
-                          <button
+                          <button id='saveExpense'
                             onClick={() => saveExpense(expense)}
                           >
                             <FiCheck 
                             className="text-green-500 w-5 h-5"/> 
                           </button>
+                          
                           <button 
+                            id="cancelExpenseEdit"
                             onClick={() => cancelEditExp()}
                           >
                           
@@ -322,11 +395,13 @@ const ExpenseTrackerPage = () => {
                 }
               </tbody>
           </table> 
+          : <p className="text-center"> No hay gastos para esta semama </p>
+          }
 {/* Expense Table */}
-        <div>
-        </div>
 {/* Pagination */}                           
-       <div className="flex justify-between mt-8">
+        {expenses.length > 0 &&
+
+       <div className="flex justify-between mt-8" id="pagination">
         <p className="text-sm text-gray-700"> { `Pagina ${pagination.actPage}  de ${pagination.totalPages} de ${pagination.totalExpenes}`}</p>
         <div className="flex space-x-2">
           <button className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:bg-gray-100 disabled:cursor-not-allowed">
@@ -337,7 +412,9 @@ const ExpenseTrackerPage = () => {
           </button>
        </div>
 
-       </div>          
+       </div> 
+       
+      }         
 
 {/* Settings Sidebar */}
         {isOpenSettings && (
